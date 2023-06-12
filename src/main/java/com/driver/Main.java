aa946b55415b6579543178b3f491fb1f49b59663
Anoop kushwaha <anoopkushwaha509@gmail.com>
2023-06-12 14:52:08 -0400

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Anoop"; // Error: Cannot access private member
+//        System.out.println(obj.name); // Error: Cannot access private member
+
+        obj.setName("Anoop");
+        String name = obj.getName();
+        System.out.println(name);
+
+
+    }
 }
